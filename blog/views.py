e59566ebc87dde0c066352b6eb189a6a837c57fd
Chris.C <chris.c@ubixeng.com>
2018-06-29 09:34:08 -0400

    Blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Blog
+
+def allBlogs(request):
+    blogs = Blog.objects
+    return render(request, 'blogs/allBlogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    detailBlog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blogs/detail.html', {'blog':detailBlog})
